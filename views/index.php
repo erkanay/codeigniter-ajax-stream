6ecd8cee8506dd61ef5ff2ad9a7cebab9db870da
@erkan <erkanay@users.noreply.github.com>
2015-01-20 18:58:11 +0500

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,4 +1,48 @@
-{categories}
-    {c_id} - {name}
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <style>
+            .myform{
+                width:600px;margin: 0 auto;
+            }
+            .myform select{
+                max-width: 120px;
+            }
+            .post-stream{
+                width:600px;margin:0 auto;
+            }
+        </style>
+    </head>
+<body>
+    <form action="" method="post" class="myform">
+        <span class="label label-default">Post</span>
+        <textarea class="form-control" rows="8" cols="50" name="post"></textarea><br>
+        <span class="label label-default">Category</span>
+        <select class="form-control" name="category">
+            {categories}
+            <option value={c_id}>{name}</option>
+            {/categories}
+        </select>
+    </form>
     <br>
-{/categories}
+    <div class="post-stream">
+        <span class="label label-primary">Posts</span>
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+</body>
+</html>
+ 
