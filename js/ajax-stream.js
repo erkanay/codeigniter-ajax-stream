ae38edb643a5dfe337fe01261bd32affcaf72273
@erkan <erkanay@users.noreply.github.com>
2015-01-24 23:52:25 +0500

    Update ajax-stream.js

--- js/ajax-stream.js
+++ js/ajax-stream.js
@@ -5,3 +5,34 @@
  * @date   : 24.01.2015
  *
  */
+$.jQuery(function(){
+  /**
+   * @var state
+   * 
+   */
+  var state = false;
+  /**
+   * @func. ajax method 
+   * 
+   */
+  var request = $.ajax({
+      url: "script.php",
+      type: "POST",
+      data: { id : menuId },
+      dataType: "html"
+  });
+  /**
+   * ajax binding
+   * 
+   */   
+  request.done(function( msg ) {
+      $( "#log" ).html( msg );
+  });
+  /**
+   * ajax fail
+   * 
+   */   
+  request.fail(function( jqXHR, textStatus ) {
+      alert( "Request failed: " + textStatus );
+  });  
+});
